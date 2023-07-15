4d4eb78edb154f093119bae675a755cf7ba04612
ken60k <ken.lov.f1sau@gmail.com>
2023-07-16 01:21:19 +0900

    cargo build test

---
+++ test_build/target/debug/deps/test_build-9d4c53e05036336e.d
@@ -0,0 +1,5 @@
+/home/ken60/study_rust/test_build/target/debug/deps/test_build-9d4c53e05036336e: src/main.rs
+
+/home/ken60/study_rust/test_build/target/debug/deps/test_build-9d4c53e05036336e.d: src/main.rs
+
+src/main.rs:
---
+++ test_build/target/debug/test_build.d
@@ -0,0 +1,1 @@
+/home/ken60/study_rust/test_build/target/debug/test_build: /home/ken60/study_rust/test_build/src/main.rs
