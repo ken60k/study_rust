8654f5bbdece61c3704dc88a93ab1b53f6d14767
ken60k <ken.lov.f1sau@gmail.com>
2023-07-15 22:23:30 +0900

    cargo run test

---
+++ test_run/target/debug/deps/test_run-8859e747c9a33ab7.d
@@ -0,0 +1,5 @@
+/home/ken60/study_rust/test_run/target/debug/deps/test_run-8859e747c9a33ab7: src/main.rs
+
+/home/ken60/study_rust/test_run/target/debug/deps/test_run-8859e747c9a33ab7.d: src/main.rs
+
+src/main.rs:
---
+++ test_run/target/debug/test_run.d
@@ -0,0 +1,1 @@
+/home/ken60/study_rust/test_run/target/debug/test_run: /home/ken60/study_rust/test_run/src/main.rs
